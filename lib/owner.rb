06b30de9ffc8f8c0b558881e7c63d1cedede30ca
amanda <amanda.blaze1988@gmail.com>
2020-12-06 17:17:04 -0800

    commit tree bd13c16a8961676235c107529ff5bbfcdb97bb40

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :species, :name 
+  
+  @@all = []
+  
+  def initialize(name, pets = { :dogs => [], :cats => []})  
+    @name = name
+    @species = "human"
+    @pets = {:dog => [], :cat => []}
+    @@all << self
+  end 
+
+  def say_species
+    "I am a #{@species}."
+  end 
+
+  def cats 
+    @pets[:cats] 
+  end
+  
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self. count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
+
